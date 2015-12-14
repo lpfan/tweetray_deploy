7e40bc139e04a76c0852a016317a334a55d79433
rudy <misha.shchetinin@gmail.com>
2015-12-14 15:19:37 -0500

    added downloading of mongodb jsr

--- cookbooks/tweetray/recipes/spark.rb
+++ cookbooks/tweetray/recipes/spark.rb
@@ -10,3 +10,13 @@ tar_extract 'http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz'
     target_dir '/usr/local/spark'
     tar_flags ['--strip-components 1']
 end
+
+remote_file '/usr/local/spark/lib/mongo-hadoop-core.jar' do
+    source 'http://central.maven.org/maven2/org/mongodb/mongo-hadoop-core/1.3.0/mongo-hadoop-core-1.3.0.jar'
+    mode '0755'
+end
+
+remote_file '/usr/local/spark/lib/mongo-java-driver.jar' do
+    source 'http://central.maven.org/maven2/org/mongodb/mongo-java-driver/3.2.0/mongo-java-driver-3.2.0.jar'
+    mode '0755'
+end
