23ee60b57fd96675a99b9b017c108a6f15fa5901
rudy <misha.shchetinin@gmail.com>
2015-12-16 16:05:31 -0500

    added pymongo-spark to be installed

--- cookbooks/tweetray/recipes/start_app.rb
+++ cookbooks/tweetray/recipes/start_app.rb
@@ -18,3 +18,18 @@ bash 'create, activate venv and install all reqs' do
     /opt/tweetray/env/bin/pip install -r reqs.txt
     EOH
 end
+
+git '/tmp/mongo-hadoop' do
+    repository 'https://github.com/mongodb/mongo-hadoop.git'
+    revision 'master'
+    action :sync
+    user 'rab'
+end
+
+bash 'install pymongo-spark' do
+    cwd '/tmp/mongo-hadoop/spark/src/main/python'
+    user 'rab'
+    code <<-EOH
+    /opt/tweetray/env/bin/python setup.py install
+    EOH
+end
